15c6f038b7ec171fd88fdf2f33e6504203bfa40b
MohammadSalemKhalaf <s12112969@stu.najah.edu>
2024-08-25 11:41:56 -0700

    Update AccountManagementStep.java

--- src/test/java/acceptance/AccountManagementStep.java
+++ src/test/java/acceptance/AccountManagementStep.java
@@ -26,17 +26,17 @@ public class AccountManagementStep {
 	public void i_navigate_to_the_account_management_page() {
 	    app.navigateTo("account management");
 	}
-	@Then("I should see my account details")
-	public void i_should_see_my_account_details() {
-	    app.showAccountDetails();
-	}
+//	@Then("I should see my account details")
+//	public void i_should_see_my_account_details() {
+//	    app.showAccountDetails();
+//	}
 
 	
-	@When("I navigate to the account management page And I select {string} and I enter username {string} and pass {string}  update the business information")
-	public void i_navigate_to_the_account_management_page_and_i_select_and_i_update_the_business_information(String op,String accName,String password) {
-	   app.navigateTo("account management");
-	   app.EditBusinessInformation(op,accName,password);
-	}
+//	@When("I navigate to the account management page And I select {string} and I enter username {string} and pass {string}  update the business information")
+//	public void i_navigate_to_the_account_management_page_and_i_select_and_i_update_the_business_information(String op,String accName,String password) {
+//	   app.navigateTo("account management");
+//	   app.EditBusinessInformation(op,accName,password);
+//	}
 	@Then("I should see the updated information on the account management page")
 	public void i_should_see_the_updated_information_on_the_account_management_page() {
 	    assertTrue(true);
