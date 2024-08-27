8bc412079450ae19b7d4ffe043a2039a684d7ee1
MohammadSalemKhalaf <s12112969@stu.najah.edu>
2024-08-26 17:28:05 -0700

    Update CommunicationAndFeedbackTest.java

--- src/test/java/acceptance/CommunicationAndFeedbackTest.java
+++ src/test/java/acceptance/CommunicationAndFeedbackTest.java
@@ -23,41 +23,33 @@ public class CommunicationAndFeedbackTest {
 	}
 	@Given("I am on the user dashboard")
 	public void i_am_on_the_user_dashboard() {
-//    app.user.UserDashboard();
-//    app.user.printDashboard();
-    assertTrue(true);
-
+    app.user.UserDashboard();
+    app.user.printDashboard();
 	}
-//	@When("I choose to send a message to {string} and enter the message {string}")
+	@When("I choose to send a message to {string} and enter the message {string}")
 	public void i_choose_to_send_a_message_to_and_enter_the_message(String recipientType, String message) {
-//		app.user.chatWithOwner_Supplier( recipientType,message);
-//	}
-//	@Then("return back to the user dashboard")
-//	public void return_back_to_the_user_dashboard() {
-//		  app.user.UserDashboard();
-//		    app.user.printDashboard();
-	    assertTrue(true);
-
+		app.user.chatWithOwner_Supplier( recipientType,message);
+	}
+	@Then("return back to the user dashboard")
+	public void return_back_to_the_user_dashboard() {
+		  app.user.UserDashboard();
+		    app.user.printDashboard();
 	}
 	@Given("I have received a purchased dessert")
 	public void i_have_received_a_purchased_dessert() {
 	    app.login("musa", "123", "user");
 
-//    app.user.purchaseProduct("milk", 3);
+    app.user.purchaseProduct("milk", 3);
     //assertTrue(app.user.receivedAPurchasedDessert);
-    assertTrue(true);
+//    assertTrue(true);
 	}
 	@When("I navigate to the My Orders page")
 	public void i_navigate_to_the_my_orders_page() {
-//app.user.printAllOrdersForCurrentUser();
-	    assertTrue(true);
-
+app.user.printAllOrdersForCurrentUser();
 	}
 	@When("I select the dessert to provide feedback on and enter feedback message {string}")
 	public void i_select_the_dessert_to_provide_feedback_on_and_enter_feedback_message(String string) {
-	    assertTrue(true);
-
-//		app.user.sendFeedback(string);
+     app.user.sendFeedback(string);
 	}
 	
 	
